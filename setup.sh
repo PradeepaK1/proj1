d0be25c44e0c9ac578a1290dbc100c8a0566c9fd
PradeepaK1 <40286688+PradeepaK1@users.noreply.github.com>
2020-07-10 15:24:10 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,4 @@ echo "\
 [general]\n\
 email = \"ptljkpd@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = 8502\n\
-" > ~/.streamlit/config.toml
+
